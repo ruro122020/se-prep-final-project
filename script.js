35a235f1145ba971cb5f94602de2e285b1e57f3c
RuthRojas <ruro122020@gmail.com>
2023-07-11 14:35:11 -0600

    add div boxes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by JavaScript";
 // document.querySelector("body").appendChild(h2)
+const button = document.getElementById("button")
+
+button.addEventListener("click", changeFact)
+
+function changeFact(event){
+  console.log('event', event)
+
+}
+function show(){}
+function hide(){}
+function getFacts(){
+
+}

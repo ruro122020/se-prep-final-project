a205399f96af77283bbe9073a2e98de490e95188
RuthRojas <ruro122020@gmail.com>
2023-07-12 15:13:33 -0600

    create changeJoke function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const button = document.getElementById("factsButton");
-button.addEventListener("click", changeFact);
+const factsButton = document.getElementById("factsButton");
+const jokesButton = document.getElementById("jokesButton");
 const factsArray = [
   `The "sixth sick sheik's sixth sheep's sick" is believed to be the toughest tongue twister in the English language.`,
    `Wearing headphones for just an hour could increase the bacteria in your ear by 700 times.`, 
@@ -7,12 +7,16 @@ const factsArray = [
    `"Dreamt" is the only English word that ends in the letters "mt".`,
    `Bugs breath through holes on the sides of their bodies called sphericals`,
 ]
+const jokesArray = ['joke-2', 'joke-3', 'joke-4']
 let count = 0;
-function changeFact(){
 
-  const h1Child = document.getElementById("h1Facts");
+factsButton.addEventListener("click", changeFact);
+jokesButton.addEventListener("click", changeJoke)
+
+function changeFact(){
   const facts = document.getElementsByClassName('facts');
   const newFact = document.createElement('div');
+  const h1Child = document.getElementById("h1Facts");
   //count resets to zero once it hits the end of the array
   if(count === factsArray.length){
     count=0;
@@ -27,4 +31,25 @@ function changeFact(){
     h1Child.parentNode.insertBefore(newFact, h1Child.nextSibling);
     //increase count to go to the next fact in the factsArray
     count++
+}
+
+function changeJoke(){
+  const joke = document.getElementsByClassName("jokes");
+  const newJoke = document.createElement("div");
+  const h1Child = document.getElementById("h1Jokes");
+  //count resets to zero once it hits the end of the array
+  if(count === jokesArray.length){
+    count = 0
+  }
+  //remove current joke element showing
+  joke[0].remove();
+  //give new element a class name
+  newJoke.className = 'jokes';
+  //add text to new element
+  newJoke.textContent = jokesArray[count];
+  //append new element
+  h1Child.parentNode.insertBefore(newJoke, h1Child.nextSibling)
+  count++
+
+
 }
